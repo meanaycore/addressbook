0ee986010466fca80acbed4500f28cbe77837db5
akhona <Aycore411@gmail.com>
2015-03-09 07:19:02 +0200

    initial commit

---
+++ src/test/java/addressbook/AddressbookApplicationTests.java
@@ -0,0 +1,18 @@
+package addressbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AddressbookApplication.class)
+@WebAppConfiguration
+public class AddressbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
